91d2cee5353da8ead22c25d794525b62b8e02e57
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-12 21:18:53 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -10,9 +10,9 @@ puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
 puts "Step 2: Left arm out in front of you, palm facing down."
-puts "Step 2: Turn your right palm up."
 
 # Step 3: FIXME
+puts "Step 3: Turn your right palm up."
 puts "Step 3: Place your right hand on your left shoulder."
 
 # Step 4: FIXME
